ef6776b1a3213d8a324b7536f578ec9d73ce8b01
samjanderson <sam.andersonnn@live.ca>
2021-03-13 13:37:26 -0800

    add nba api request

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,38 +1,81 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, {Component} from 'react';
+import axios from "axios";
 
 class App extends Component {
-  constructor(props) {
+  constructor(props){
     super(props)
-    this.state = {
-      message: 'Click the button to load data!'
+    this.state={
+      playerName: null,
+      playerStats: {}
     }
   }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
+handleSubmit = (e) => {
+  e.preventDefault();
+  this.getPlayerId()
+  console.log(this.state.playerName)
+}
 
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
-    );
+handleChange = (event) => {
+  const replace = event.target.value.split(" ").join("_");
+  if(replace.length > 0){
+    this.setState({playerName: replace})
+  } else {
+    alert("Please type players name!")
   }
 }
 
+  getPlayerId = () => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
+    .then(async res => {
+      // console.log(res.data.data)
+      if(res.data.data[0] === undefined){
+        alert("This player is either injured or hasn't played yet!")
+      } else if(res.data.data.length > 1){
+        alert("Pleases specify the name more!")
+      } else{
+        await this.getPlayerStats(res.data.data[0].id)
+
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  getPlayerStats = (playerId) => {
+    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2006&player_ids[]=${playerId}`)
+    .then(async res => {
+      console.log(res.data.data)
+      this.setState({ playerStats: res.data.data[0]})
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  
+  render(){
+  return (
+    <div className="App">
+     <form onSubmit={this.handleSubmit}>
+       <label>
+         Name
+         <input 
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+          placeholder="please enter players name"
+         />
+       </label>
+       <input type="submit" value="Submit"/>
+     </form>
+     games played: {this.state.playerStats["games_played"]}
+     <br />
+     points averaged: {this.state.playerStats["pts"]}
+     <br />
+     rebounds averaged: {this.state.playerStats["reb"]}
+     <br />
+     assists averaged: {this.state.playerStats["ast"]}
+    </div>
+  );
+}
+}
 export default App;
