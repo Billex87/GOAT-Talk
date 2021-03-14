9dd9d946907cca73f032f3d7e8628d58dd33c829
samjanderson <sam.andersonnn@live.ca>
2021-03-14 01:42:46 +0000

    making progress

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,89 +1,17 @@
 import React, {useState} from 'react';
 import axios from "axios";
+import Player from "./components/Player.js"
+import PlayerTwo from "./components/PlayerTwo.js"
+import './App.css';
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    playerName: null,
-    playerStats: {}
-  });
-
-const handleSubmit = (e) => {
-  console.log("me dumb!")
-  e.preventDefault();
-  getPlayerId()
-  console.log(state.playerName)
-}
-
-const handleChange = (event) => {
-  const replace = event.target.value.split(" ").join("_");
-  if(replace.length > 0){
-    setState((prev) => ({
-      ...prev,
-      playerName: replace
-    }));
-  } else {
-    alert("Please type players name!")
-  }
-}
-
-  const getPlayerId = () => {
-    axios.get(`https://www.balldontlie.io/api/v1/players?search=${state.playerName}`)
-    .then(async res => {
-      // console.log(res.data.data)
-      if(typeof res.data.data[0] === "undefined"){
-        alert("This player is either injured or hasn't played yet!")
-      } else if(res.data.data.length > 1){
-        alert("Pleases specify the name more!")
-      } else{
-        await getPlayerStats(res.data.data[0].id)
-
-      }
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-
-  const getPlayerStats = (playerId) => {
-    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2006&player_ids[]=${playerId}`)
-    .then(async res => {
-      console.log(res.data.data)
-      setState((prev) => ({
-        ...prev,
-        playerStats: res.data.data[0]
-      }));
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-
-  // useEffect(() => {
-
-  //   }, []);
-
-
-  
   return (
     <div className="App">
-     <form onSubmit={handleSubmit}>
-       <label>
-         Name
-         <input 
-          type="text"
-          value={state.value}
-          onChange={handleChange}
-          placeholder="please enter players name"
-         />
-       </label>
-       <input type="submit" value="Submit"/>
-     </form>
-     games played: {state.playerStats["games_played"]}
-     <br />
-     points averaged: {state.playerStats["pts"]}
-     <br />
-     rebounds averaged: {state.playerStats["reb"]}
-     <br />
-     assists averaged: {state.playerStats["ast"]}
+      <div className="Vs">
+      <Player/>
+      <PlayerTwo/>
+      </div>
     </div>
   );
 }
---
+++ react-front-end/src/components/Player.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import axios from "axios";
+
+export default function Player(props) {
+
+  const [state, setState] = useState({
+    playerName: null,
+    playerStats: {}
+  });
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  getPlayerId()
+  console.log(state.playerName)
+}
+
+const handleChange = (event) => {
+  const replace = event.target.value.split(" ").join("_");
+  if(replace.length > 0){
+    setState((prev) => ({
+      ...prev,
+      playerName: replace
+    }));
+  } else {
+    alert("Please type players name!")
+  }
+}
+
+  const getPlayerId = () => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${state.playerName}`)
+    .then(async res => {
+      // console.log(res.data.data)
+      if(typeof res.data.data[0] === "undefined"){
+        alert("This player is either injured or hasn't played yet!")
+      } else if(res.data.data.length > 1){
+        alert("Pleases specify the name more!")
+      } else{
+        await getPlayerStats(res.data.data[0].id)
+
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  const getPlayerStats = (playerId) => {
+    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2006&player_ids[]=${playerId}`)
+    .then(async res => {
+      console.log(res.data.data)
+      setState((prev) => ({
+        ...prev,
+        playerStats: res.data.data[0]
+      }));
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  return (
+<div className="Player">
+<form onSubmit={handleSubmit}>
+  <label>
+    Name
+    <input 
+     type="text"
+     value={state.value}
+     onChange={handleChange}
+     placeholder="please enter players name"
+    />
+  </label>
+  <input type="submit" value="Submit"/>
+</form>
+Games Played: {state.playerStats["games_played"]}
+<br />
+Points Averaged: {state.playerStats["pts"]}
+<br />
+Rebounds Averaged: {state.playerStats["reb"]}
+<br />
+Assists Averaged: {state.playerStats["ast"]}
+</div>
+  )
+}
