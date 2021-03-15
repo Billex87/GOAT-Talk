d0251485fc8b6470672a470dd42d2d6b648efb91
Billy McKinnon <Billy_McKinnon@hotmail.com>
2021-03-14 17:19:52 -0700

    styled graphs

--- react-front-end/src/components/PlayerTwo.js
+++ react-front-end/src/components/PlayerTwo.js
@@ -32,8 +32,110 @@ export default function Player(props) {
   options: {
     chart: {
       type: 'bar',
-      height: 350
+      height: 350,
+      animations: {
+        enabled: true,
+        easing: 'easeinout',
+        speed: 800,
+        animateGradually: {
+            enabled: true,
+            delay: 150
+        },
+        dynamicAnimation: {
+            enabled: true,
+            speed: 350
+        }
+    }
     },
+//     theme: {
+//       mode: 'light', 
+//       palette: 'palette1', 
+//       monochrome: {
+//           enabled: false,
+//           color: '#255aee',
+//           shadeTo: 'light',
+//           shadeIntensity: 0.65
+//       },
+//   },
+//   theme: {
+//     mode: 'dark', 
+//     palette: 'palette1', 
+//     monochrome: {
+//         enabled: false,
+//         color: '#2983FF',
+//         shadeTo: 'dark',
+//         shadeIntensity: 0.65
+//     },
+// },
+yaxis: {
+  show: true,
+  showAlways: true,
+  showForNullSeries: true,
+  seriesName: undefined,
+  opposite: false,
+  reversed: false,
+  logarithmic: false,
+  tickAmount: 10,
+  min: 0,
+  max: 28,
+  forceNiceScale: true,
+  floating: false,
+  decimalsInFloat: undefined,
+  labels: {
+      show: true,
+      align: 'right',
+      minWidth: 0,
+      maxWidth: 160,
+      style: {
+          colors: [],
+          fontSize: '12px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 400,
+          cssClass: 'apexcharts-yaxis-label',
+      },
+  },
+  axisBorder: {
+      show: true,
+      color: '#78909C',
+      offsetX: 0,
+      offsetY: 0
+  },
+  axisTicks: {
+      show: true,
+      borderType: 'solid',
+      color: '#78909C',
+      width: 6,
+      offsetX: 0,
+      offsetY: 0
+  },
+  title: {
+      text: undefined,
+      rotate: -90,
+      offsetX: 0,
+      offsetY: 0,
+      style: {
+          color: undefined,
+          fontSize: '12px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 600,
+          cssClass: 'apexcharts-yaxis-title',
+      },
+  },
+  crosshairs: {
+      show: true,
+      position: 'back',
+      stroke: {
+          color: '#b6b6b6',
+          width: 1,
+          dashArray: 0,
+      },
+  },
+  tooltip: {
+      enabled: true,
+      offsetX: 0,
+  },
+  
+},
     plotOptions: {
       bar: {
         horizontal: true,
@@ -47,10 +149,44 @@ export default function Player(props) {
     },
     dataLabels: {
       enabled: true,
-      offsetX: -6,
+      enabledOnSeries: undefined,
+      formatter: function (val, opts) {
+          return val
+      },
+      textAnchor: 'middle',
+      distributed: false,
+      offsetX: 0,
+      offsetY: 0,
       style: {
-        fontSize: '12px',
-        colors: ['#000']
+          fontSize: '14px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 'bold',
+          colors: undefined
+      },
+      background: {
+        enabled: true,
+        foreColor: '#fff',
+        padding: 4,
+        borderRadius: 2,
+        borderWidth: 1,
+        borderColor: '#fff',
+        opacity: 0.9,
+        dropShadow: {
+          enabled: false,
+          top: 1,
+          left: 1,
+          blur: 1,
+          color: '#000',
+          opacity: 0.45
+        }
+      },
+      dropShadow: {
+          enabled: false,
+          top: 1,
+          left: 1,
+          blur: 1,
+          color: '#000',
+          opacity: 0.45
       }
     },
     stroke: {
