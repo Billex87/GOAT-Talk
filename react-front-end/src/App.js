73f86ad57f7ef4a100174eab0e508984d62ddf90
samjanderson <sam.andersonnn@live.ca>
2021-03-15 15:40:45 -0700

    Got State to parent

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,28 +1,121 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from "axios";
-import Player from "./components/Player.js"
-import PlayerTwo from "./components/PlayerTwo.js"
+import Player from "./components/Player.js";
+import PlayerTwo from "./components/PlayerTwo.js";
 import './App.css';
 
 export default function Application(props) {
 
+  const [state, setState] = useState({
+    playerOneName: null,
+    playerOneStats: { "pts": 0, "reb": 0, "ast": 0, "stl": 0, "blk": 0, "fg_pct": 0, "fg3_pct": 0, "ft_pct": 0 },
+    seasonOne: null,
+    firstNameOne: null,
+    lastNameOne: null,
+    positionOne: null,
+    teamOne: null,
+    yearOne: null,
+    playerTwoName: null,
+    playerTwoStats: { "pts": 0, "reb": 0, "ast": 0, "stl": 0, "blk": 0, "fg_pct": 0, "fg3_pct": 0, "ft_pct": 0 },
+    seasonTwo: null,
+    firstNameTwo: null,
+    lastNameTwo: null,
+    positionTwo: null,
+    teamTwo: null,
+    yearTwo: null
+  });
+
+  const getPlayerOne = (name, season) => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${name}`)
+      .then(async res => {
+        // console.log(res.data.data)
+        if (typeof res.data.data[0] === "undefined") {
+          alert("This player is either injured or hasn't played yet!");
+        } else if (res.data.data.length > 1) {
+          alert("Pleases specify the name more!");
+        } else {
+          axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${res.data.data[0].id}`)
+            .then(async res => {
+              console.log(res.data.data);
+              setState((prev) => ({
+                ...prev,
+                playerOneStats: res.data.data[0],
+                yearOne: res.data.data[0].season
+              }));
+            }).catch(err => {
+              console.log(err);
+            });
+          setState((prev) => ({
+            ...prev,
+            firstNameOne: res.data.data[0].first_name,
+            lastNameOne: res.data.data[0].last_name,
+            positionOne: res.data.data[0].position,
+            teamOne: res.data.data[0].team.abbreviation,
+
+          }));
+
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+
+  };
+  const getPlayerTwo = (name, season) => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${name}`)
+      .then(async res => {
+        // console.log(res.data.data)
+        if (typeof res.data.data[0] === "undefined") {
+          alert("This player is either injured or hasn't played yet!");
+        } else if (res.data.data.length > 1) {
+          alert("Pleases specify the name more!");
+        } else {
+          axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${res.data.data[0].id}`)
+            .then(async res => {
+              console.log(res.data.data);
+              setState((prev) => ({
+                ...prev,
+                playerTwoStats: res.data.data[0],
+                yearTwo: res.data.data[0].season
+              }));
+            }).catch(err => {
+              console.log(err);
+            });
+          setState((prev) => ({
+            ...prev,
+            firstNameTwo: res.data.data[0].first_name,
+            lastNameTwo: res.data.data[0].last_name,
+            positionTwo: res.data.data[0].position,
+            teamTwo: res.data.data[0].team.abbreviation,
+
+          }));
+
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+
+  };
+
+
+
+
   return (
     <div className="App">
       <div className="Vs">
-      <Player/>
-    <section className="Tetris">
-    <p>PPG</p>
-    <p>RPG</p>
-    <p>APG</p>
-    <p>SPG</p>
-    <p>BPG</p>
-    <p>   </p>
-    <p>   </p>
-    <p>FG%</p>
-    <p>3PT%</p>
-    <p>FT%</p>
-    </section>
-      <PlayerTwo/>
+        <Player getPlayerOne = {getPlayerOne} {...state}/>
+        <section className="Tetris">
+          <p>PPG</p>
+          <p>RPG</p>
+          <p>APG</p>
+          <p>SPG</p>
+          <p>BPG</p>
+          <p>   </p>
+          <p>   </p>
+          <p>FG%</p>
+          <p>3PT%</p>
+          <p>FT%</p>
+        </section>
+        <PlayerTwo getPlayerTwo = {getPlayerTwo} {...state} />
       </div>
     </div>
   );
--- react-front-end/src/components/PlayerTwo.js
+++ react-front-end/src/components/PlayerTwo.js
@@ -4,85 +4,89 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import SportsBasketballIcon from '@material-ui/icons/SportsBasketball';
 import './Player.css';
-import Chart from 'react-apexcharts'
+import Chart from 'react-apexcharts';
 
 export default function Player(props) {
 
   const [state, setState] = useState({
     playerName: null,
-    playerStats: {"pts":0,"reb":0,"ast":0,"stl":0,"blk":0,"fg_pct":0,"fg3_pct":0,"ft_pct":0},
     season: null,
-    firstName: null,
-    lastName: null,
-    position: null,
-    team: null,
   });
   const stats = {
     series: [{
-    data: [state.playerStats["pts"], state.playerStats["reb"], state.playerStats["ast"], state.playerStats["stl"], state.playerStats["blk"]]
-  }],
-  options: {
-    yaxis: {
-  max: 28,
-
-  forceNiceScale: true,
-  floating: false,
-  decimalsInFloat: undefined,
-  labels: {
-      show: true,
-      align: 'right',
-      minWidth: 0,
-      maxWidth: 160,
-      style: {
-          colors: [],
-          fontSize: '12px',
-          fontFamily: 'Helvetica, Arial, sans-serif',
-          fontWeight: 400,
-          cssClass: 'apexcharts-yaxis-label',
+      data: [props.playerTwoStats["pts"], props.playerTwoStats["reb"], props.playerTwoStats["ast"], props.playerTwoStats["stl"], props.playerTwoStats["blk"]]
+    }],
+    options: {
+      yaxis: {
+        max: 28,
+        reversed: true,
+        forceNiceScale: true,
+        floating: false,
+        decimalsInFloat: undefined,
+        labels: {
+          show: true,
+          align: 'right',
+          minWidth: 0,
+          maxWidth: 160,
+          style: {
+            colors: [],
+            fontSize: '12px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 400,
+            cssClass: 'apexcharts-yaxis-label',
+          },
+        },
+        axisBorder: {
+          show: true,
+          color: '#78909C',
+          offsetX: 0,
+          offsetY: 0
+        }
       },
-  },
-  axisBorder: {
-      show: true,
-      color: '#78909C',
-      offsetX: 0,
-      offsetY: 0
-  }
-  },
-    plotOptions: {
-      bar: {
-        horizontal: true,
-      }
-    },
-    dataLabels: {
-      enabled: false
-    },
-    xaxis: {
-      categories: ['', '', '', '', '', ''],    
-    },
-    dataLabels: {
-      enabled: true,
-      enabledOnSeries: undefined,
-      formatter: function (val, opts) {
-          return val
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        }
       },
-      textAnchor: 'middle',
-      distributed: false,
-      offsetX: 0,
-      offsetY: 0,
-      style: {
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ['', '', '', '', '', ''],
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: undefined,
+        formatter: function (val, opts) {
+          return val;
+        },
+        textAnchor: 'middle',
+        distributed: false,
+        offsetX: 0,
+        offsetY: 0,
+        style: {
           fontSize: '14px',
           fontFamily: 'Helvetica, Arial, sans-serif',
           fontWeight: 'bold',
           colors: undefined
-      },
-      background: {
-        enabled: true,
-        foreColor: '#fff',
-        padding: 4,
-        borderRadius: 2,
-        borderWidth: 1,
-        borderColor: '#fff',
-        opacity: 0.9,
+        },
+        background: {
+          enabled: true,
+          foreColor: '#fff',
+          padding: 4,
+          borderRadius: 2,
+          borderWidth: 1,
+          borderColor: '#fff',
+          opacity: 0.9,
+          dropShadow: {
+            enabled: false,
+            top: 1,
+            left: 1,
+            blur: 1,
+            color: '#000',
+            opacity: 0.45
+          }
+        },
         dropShadow: {
           enabled: false,
           top: 1,
@@ -92,103 +96,95 @@ export default function Player(props) {
           opacity: 0.45
         }
       },
-      dropShadow: {
+      stroke: {
+        show: true,
+        width: 1,
+        colors: ['#fff']
+      },
+
+    },
+  };
+  const efficiency = {
+    series: [{
+      data: [props.playerTwoStats["fg_pct"], props.playerTwoStats["fg3_pct"], props.playerTwoStats["ft_pct"]]
+    }],
+    options: {
+      yaxis: {
+        min: 0,
+        max: 1,
+        reversed: true,
+        forceNiceScale: true,
+        floating: false,
+        tickAmount: 5,
+        decimalsInFloat: undefined,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ['', '', ''],
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: undefined,
+        formatter: function (val, opts) {
+          return val;
+        },
+        textAnchor: 'middle',
+        distributed: false,
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          fontSize: '14px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 'bold',
+          colors: undefined
+        },
+        background: {
+          enabled: true,
+          foreColor: '#fff',
+          padding: 4,
+          borderRadius: 2,
+          borderWidth: 1,
+          borderColor: '#fff',
+          opacity: 0.9,
+          dropShadow: {
+            enabled: false,
+            top: 1,
+            left: 1,
+            blur: 1,
+            color: '#000',
+            opacity: 0.45
+          }
+        },
+        dropShadow: {
           enabled: false,
           top: 1,
           left: 1,
           blur: 1,
           color: '#000',
           opacity: 0.45
-      }
+        }
+      },
+      stroke: {
+        show: true,
+        width: 1,
+        colors: ['#fff']
+      },
     },
-    stroke: {
-      show: true,
-      width: 1,
-      colors: ['#fff']
-    },
-    
-  },
-};
-const efficiency = {
-  series: [{
-  data: [state.playerStats["fg_pct"], state.playerStats["fg3_pct"], state.playerStats["ft_pct"]]
-}],
-options: {
-  yaxis: {
-min: 0,
-max: 1,
+  };
 
-forceNiceScale: true,
-floating: false,
-tickAmount: 5,
-decimalsInFloat: undefined,
-},
-  plotOptions: {
-    bar: {
-      horizontal: true,
-    }
-  },
-  dataLabels: {
-    enabled: false
-  },
-  xaxis: {
-    categories: ['', '', ''],    
-  },
-  dataLabels: {
-    enabled: true,
-    enabledOnSeries: undefined,
-    formatter: function (val, opts) {
-        return val
-    },
-    textAnchor: 'middle',
-    distributed: false,
-    offsetX: 0,
-    offsetY: 0,
-    style: {
-        fontSize: '14px',
-        fontFamily: 'Helvetica, Arial, sans-serif',
-        fontWeight: 'bold',
-        colors: undefined
-    },
-    background: {
-      enabled: true,
-      foreColor: '#fff',
-      padding: 4,
-      borderRadius: 2,
-      borderWidth: 1,
-      borderColor: '#fff',
-      opacity: 0.9,
-      dropShadow: {
-        enabled: false,
-        top: 1,
-        left: 1,
-        blur: 1,
-        color: '#000',
-        opacity: 0.45
-      }
-    },
-    dropShadow: {
-        enabled: false,
-        top: 1,
-        left: 1,
-        blur: 1,
-        color: '#000',
-        opacity: 0.45
-    }
-  },
-  stroke: {
-    show: true,
-    width: 1,
-    colors: ['#fff']
-  }, 
-},
-};
-
- // Line 37 ----- PPG', 'RPG', 'APG', 'SPG', 'BPG', 'FG%','3PT%', 'FT%' - Graph Order
+  // Line 37 ----- PPG', 'RPG', 'APG', 'SPG', 'BPG', 'FG%','3PT%', 'FT%' - Graph Order
   const handleSubmit = (e) => {
     e.preventDefault();
-    getPlayerId();
-    console.log(state.playerName);
+    props.getPlayerTwo(state.playerName, state.season);
+    // getPlayerId();
+    // console.log(state.playerName);
   };
 
   const handleChange = (event) => {
@@ -214,49 +210,11 @@ decimalsInFloat: undefined,
     }
   };
 
-  const getPlayerId = () => {
-    axios.get(`https://www.balldontlie.io/api/v1/players?search=${state.playerName}`)
-      .then(async res => {
-        // console.log(res.data.data)
-        if (typeof res.data.data[0] === "undefined") {
-          alert("This player is either injured or hasn't played yet!");
-        } else if (res.data.data.length > 1) {
-          alert("Pleases specify the name more!");
-        } else {
-          await getPlayerStats(res.data.data[0].id, state.season);
-          setState((prev) => ({
-            ...prev,
-            firstName: res.data.data[0].first_name,
-            lastName: res.data.data[0].last_name,
-            position: res.data.data[0].position,
-            team: res.data.data[0].team.abbreviation,
-          }));
-
-        }
-      }).catch(err => {
-        console.log(err);
-      });
-  };
-
-  const getPlayerStats = (playerId, season) => {
-    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${playerId}`)
-      .then(async res => {
-        console.log(res.data.data);
-        setState((prev) => ({
-          ...prev,
-          playerStats: res.data.data[0],
-          year: res.data.data[0].season
-        }));
-      }).catch(err => {
-        console.log(err);
-      });
-  };
-
   return (
     <div className="Player">
       <img className="playerImage2" src="images/lebron.png" alt="Lebron" />
       <form onSubmit={handleSubmit}>
-        <SportsBasketballIcon className="ball2"/>
+        <SportsBasketballIcon className="ball" />
         <label>
           <TextField
             id="standard-basic"
@@ -273,11 +231,11 @@ decimalsInFloat: undefined,
         </label>
         <Button type="submit" value="Submit" variant="contained" color="primary">Submit</Button>
       </form>
-      {state.firstName} {state.lastName} {state.position} <br />
-      {state.team} {state.year} 
+      {props.firstNameTwo} {props.lastNameTwo} {props.positionTwo} <br />
+      {props.teamTwo} {props.yearTwo}
       <br />
-    {<Chart options={stats.options} series={stats.series} type="bar" height={350} />}
-    {<Chart options={efficiency.options} series={efficiency.series} type="bar" height={210} />}
-</div>
-);
+      {<Chart options={stats.options} series={stats.series} type="bar" height={350} />}
+      {<Chart options={efficiency.options} series={efficiency.series} type="bar" height={210} />}
+    </div>
+  );
 }
