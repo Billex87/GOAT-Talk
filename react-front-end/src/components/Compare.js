df0438124641705788930ba6632681230da795eb
samjanderson <sam.andersonnn@live.ca>
2021-03-16 18:26:42 -0700

    working on home page

---
+++ react-front-end/src/components/Compare.js
@@ -0,0 +1,240 @@
+import React, { useState } from 'react';
+import axios from "axios";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import SportsBasketballIcon from '@material-ui/icons/SportsBasketball';
+import './Compare.css';
+import Chart from 'react-apexcharts';
+
+export default function Compare(props) {
+
+  const [state, setState] = useState({
+    playerName: null,
+    season: null,
+  });
+  const stats = {
+    series: [{
+      data: [props.playerStats["pts"], props.playerStats["reb"], props.playerStats["ast"], props.playerStats["stl"], props.playerStats["blk"]]
+    }],
+    options: {
+      yaxis: {
+        max: 28,
+        reversed: props.reversed,
+        forceNiceScale: true,
+        floating: false,
+        decimalsInFloat: undefined,
+        labels: {
+          show: true,
+          align: 'right',
+          minWidth: 0,
+          maxWidth: 160,
+          style: {
+            colors: [],
+            fontSize: '12px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 400,
+            cssClass: 'apexcharts-yaxis-label',
+          },
+        },
+        axisBorder: {
+          show: true,
+          color: '#78909C',
+          offsetX: 0,
+          offsetY: 0
+        }
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ['', '', '', '', '', ''],
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: undefined,
+        formatter: function (val, opts) {
+          return val;
+        },
+        textAnchor: 'middle',
+        distributed: false,
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          fontSize: '14px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 'bold',
+          colors: undefined
+        },
+        background: {
+          enabled: true,
+          foreColor: '#fff',
+          padding: 4,
+          borderRadius: 2,
+          borderWidth: 1,
+          borderColor: '#fff',
+          opacity: 0.9,
+          dropShadow: {
+            enabled: false,
+            top: 1,
+            left: 1,
+            blur: 1,
+            color: '#000',
+            opacity: 0.45
+          }
+        },
+        dropShadow: {
+          enabled: false,
+          top: 1,
+          left: 1,
+          blur: 1,
+          color: '#000',
+          opacity: 0.45
+        }
+      },
+      stroke: {
+        show: true,
+        width: 1,
+        colors: ['#fff']
+      },
+    },
+  };
+  const efficiency = {
+    series: [{
+      data: [props.playerStats["fg_pct"], props.playerStats["fg3_pct"], props.playerStats["ft_pct"]]
+    }],
+    options: {
+      yaxis: {
+        min: 0,
+        max: 1,
+        reversed: props.reversed,
+        forceNiceScale: true,
+        floating: false,
+        tickAmount: 5,
+        decimalsInFloat: undefined,
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        }
+      },
+      xaxis: {
+        categories: ['', '', ''],
+      },
+      dataLabels: {
+        enabled: true,
+        enabledOnSeries: undefined,
+        formatter: function (val, opts) {
+          return val;
+        },
+        textAnchor: 'middle',
+        distributed: false,
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+          fontSize: '14px',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          fontWeight: 'bold',
+          colors: undefined
+        },
+        background: {
+          enabled: true,
+          foreColor: '#fff',
+          padding: 4,
+          borderRadius: 2,
+          borderWidth: 1,
+          borderColor: '#fff',
+          opacity: 0.9,
+          dropShadow: {
+            enabled: false,
+            top: 1,
+            left: 1,
+            blur: 1,
+            color: '#000',
+            opacity: 0.45
+          }
+        },
+        dropShadow: {
+          enabled: false,
+          top: 1,
+          left: 1,
+          blur: 1,
+          color: '#000',
+          opacity: 0.45
+        }
+      },
+      stroke: {
+        show: true,
+        width: 1,
+        colors: ['#fff']
+      },
+    },
+  };
+
+  // Line 37 ----- PPG', 'RPG', 'APG', 'SPG', 'BPG', 'FG%','3PT%', 'FT%' - Graph Order
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.getPlayer(state.playerName, state.season);
+    // getPlayerId();
+    // console.log(state.playerName);
+  };
+
+  const handleChange = (event) => {
+    const replace = event.target.value.split(" ").join("_");
+    if (replace.length > 0) {
+      setState((prev) => ({
+        ...prev,
+        playerName: replace
+      }));
+    } else {
+      alert("Please Type Players Name!");
+    }
+  };
+  const handleChangeSeason = (event) => {
+    const replace = event.target.value.split(" ").join("_");
+    if (replace.length > 0) {
+      setState((prev) => ({
+        ...prev,
+        season: replace
+      }));
+    } else {
+      alert("Please Type Players Name!");
+    }
+  };
+
+  return (
+    <div className="Player">
+      <img className={props.playerImage} src={`images/${state.playerName}.png`}
+ alt="" />
+      <form onSubmit={handleSubmit}>
+        <SportsBasketballIcon className="ball" />
+        <label>
+          <TextField
+            id="standard-basic"
+            type="text"
+            value={state.value}
+            onChange={handleChange}
+            placeholder="Enter Player Name" />
+          <TextField
+            id="standard-basic"
+            type="text"
+            value={state.value}
+            onChange={handleChangeSeason}
+            placeholder="Enter Season" />
+        </label>
+        <Button type="submit" value="Submit" variant="contained" color="primary">Submit</Button>
+      </form>
+      <div className={props.nameStyle}>{props.firstName} {props.lastName}</div>
+      <div className={props.yearStyle}>{props.year}</div>
+      <div className={props.teamStyle}>{props.team} {props.position}</div>
+      <br />
+      {<Chart options={stats.options} series={stats.series} type="bar" height={350} />}
+      {<Chart options={efficiency.options} series={efficiency.series} type="bar" height={210} />}
+      {/* <img className={props.playerAwards} src={`images/${state.playerName}_awards.png`} alt="" /> */}
+    </div>
+  );
+}
--- react-front-end/src/components/home.js
+++ react-front-end/src/components/home.js
@@ -1,63 +1,125 @@
 import React, { useEffect, useState } from "react";
-import Paper from '@material-ui/core/Paper';
-import Tilt from 'react-tilt';
-import {
-  Link
-} from "react-router-dom";
-
-import './home.css'
+import axios from "axios";
+import Compare from "./Compare.js";
+import './home.css';
 
 export default function Home(props) {
 
+  const [playerOneState, setPlayerOneState] = useState({
+    playerName: null,
+    playerStats: { "pts": 0, "reb": 0, "ast": 0, "stl": 0, "blk": 0, "fg_pct": 0, "fg3_pct": 0, "ft_pct": 0 },
+    season: null,
+    firstName: null,
+    lastName: null,
+    position: null,
+    team: null,
+    year: null,
+  });
+
+  const [playerTwoState, setPlayerTwoState] = useState({
+    playerName: null,
+    playerStats: { "pts": 0, "reb": 0, "ast": 0, "stl": 0, "blk": 0, "fg_pct": 0, "fg3_pct": 0, "ft_pct": 0 },
+    season: null,
+    firstName: null,
+    lastName: null,
+    position: null,
+    team: null,
+    year: null,
+  });
+
+  const getPlayerOne = (name, season) => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${name}`)
+      .then(async res => {
+        // console.log(res.data.data)
+        if (typeof res.data.data[0] === "undefined") {
+          alert("This player is either injured or hasn't played yet!");
+        } else if (res.data.data.length > 1) {
+          alert("Please specify the name more!");
+        } else {
+          axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${res.data.data[0].id}`)
+            .then(async res => {
+              console.log(res.data.data);
+              setPlayerOneState((prev) => ({
+                ...prev,
+                playerStats: res.data.data[0],
+                year: res.data.data[0].season
+              }));
+            }).catch(err => {
+              console.log(err);
+            });
+          setPlayerOneState((prev) => ({
+            ...prev,
+            firstName: res.data.data[0].first_name,
+            lastName: res.data.data[0].last_name,
+            position: res.data.data[0].position,
+            team: res.data.data[0].team.city,
+
+          }));
+
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+
+  };
+  const getPlayerTwo = (name, season) => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${name}`)
+      .then(async res => {
+        // console.log(res.data.data)
+        if (typeof res.data.data[0] === "undefined") {
+          alert("This player is either injured or hasn't played yet!");
+        } else if (res.data.data.length > 1) {
+          alert("Pleases specify the name more!");
+        } else {
+          axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${res.data.data[0].id}`)
+            .then(async res => {
+              console.log(res.data.data);
+              setPlayerTwoState((prev) => ({
+                ...prev,
+                playerStats: res.data.data[0],
+                year: res.data.data[0].season
+              }));
+            }).catch(err => {
+              console.log(err);
+            });
+          setPlayerTwoState((prev) => ({
+            ...prev,
+            firstName: res.data.data[0].first_name,
+            lastName: res.data.data[0].last_name,
+            position: res.data.data[0].position,
+            team: res.data.data[0].team.city,
+
+          }));
+
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+
+  };
+
+
+
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: 'black', paddingTop: '20px', paddingBottom: '20px'}}>
-
-      <div style={{display: 'flex', position: 'relative', justifyContent: 'center', alignItems: 'center', flexDirection: 'row', transformStyle: 'preserve-3d'}}>
-
-      <Link to='/players' style={{textDecoration: 'none'}}>
-        <Tilt button key={'Players'} className="Tilt" options={{ max : 20, perspective: 2000, scale: 1.02 }} style={{ height: '100%', width: '100%', textDecoration: 'none'}} >
-          <Paper  style={{backgroundColor: 'black', width: '200px', height: '200px', margin: '1px 30px', borderRadius: '160px', zIndex: 1, border: 'solid black 3px'}}>
-              <div style={{height: '100%', color: 'white', display: 'flex', flexDirection: 'column'}}>
-                <p style={{position: "relative", left: "6%", top: "35%", textEmphasis: 'bold', fontSize: '15px', textShadow:
-  '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000'}}>Player Stats</p>
-                <img src={""} style={{height: '320px', width: '250px', position: 'relative', transition: '0.5s', zIndex: 11, transformStyle: 'preserve-3d', transform: 'translate3d(2%,-30%,0px)'}} alt="players" />
-              </div>
-          </Paper>
-        </Tilt>
-      </Link>
-
-      <Link to='/standings' style={{textDecoration: 'none'}}>
-        <Tilt button key={'Standings'} className="Tilt" options={{ max : 20, perspective: 2000, scale: 1.02 }} style={{ height: '100%', width: '100%' }} >
-          <Paper  style={{backgroundColor: 'black', width: '200px', height: '200px', margin: '1px 30px', borderRadius: '160px', zIndex: 1, border: 'solid black 3px'}}>
-              <div style={{height: '100%', color: 'purple', display: 'flex', flexDirection: 'column'}}>
-                <p style={{position: "relative", left: "20%", top: "55%", textEmphasis: 'bold', fontSize: '14px', textShadow:
-  '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000'}}> League Standings</p>
-                <img src={"/images/bronbron.png"} style={{height: '390px', width: '240px', position: 'relative', transition: '0.5s', zIndex: 11, transformStyle: 'preserve-3d', transform: 'translate3d(20%,-30%,0px)'}} alt="standings" />
-              </div>
-          </Paper>
-        </Tilt>
-      </Link>
-
-      <Link to='/leaders' style={{textDecoration: 'none'}}>
-        <Tilt button key={'Leaders'} className="Tilt" options={{ max : 20, perspective: 2000, scale: 1.02 }} style={{ height: '100%', width: '100%' }} >
-          <Paper  style={{backgroundColor: 'black', width: '200px', height: '200px', margin: '15px 30px', borderRadius: '160px', zIndex: 1, border: 'solid black 3px'}}>
-              <div style={{height: '100%', color: 'black', display: 'flex', flexDirection: 'column'}}>
-                <p style={{position: "relative", left: "7%", top: "25%", textShadow:
-  '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000'}}>League Leaders</p>
-                <img  style={{height: '320px', width: '250px', position: 'relative', transition: '0.5s', zIndex: 11, transformStyle: 'preserve-3d', transform: 'translate3d(0%,-25%,0px)'}} alt="leaders" />
-              </div>
-          </Paper>
-        </Tilt>
-      </Link>
+    <div className="App">
+      <div className="Vs">
+        <Compare reversed={true} playerAwards={"steph"} playerAwardsImage={"curry_awards"} playerImage={"playerImage1"} nameStyle={"name"} yearStyle={"year"} teamStyle={"team"} getPlayer={getPlayerOne} {...playerOneState} />
+        <section className="Tetris">
+          <p>PPG</p>
+          <p>RPG</p>
+          <p>APG</p>
+          <p>SPG</p>
+          <p>BPG</p>
+          <p>   </p>
+          <p>   </p>
+          <p>FG%</p>
+          <p>3PT%</p>
+          <p>FT%</p>
+        </section>
+        <Compare reversed={false} playerAwards={"lebron"} playerAwardsImage={"lebron_awards"} playerImage={"playerImage2"} nameStyle={"name2"} yearStyle={"year2"} teamStyle={"team2"} getPlayer={getPlayerTwo} {...playerTwoState} />
       </div>
-      <div>
-      </div>
-      <footer>
-        <p style={{color: 'black', textShadow: 'none'}}>@ 2021 Tale Of The Tape</p>
-      </footer>
-
-
     </div>
   );
+
 }
