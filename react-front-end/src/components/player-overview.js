bd800640f4538397d434f4a5361f09a681f14e5d
Billy McKinnon <Billy_McKinnon@hotmail.com>
2021-03-18 16:37:16 -0700

    css

--- react-front-end/src/components/player-overview.js
+++ react-front-end/src/components/player-overview.js
@@ -12,14 +12,14 @@ export default function PlayerOverview(props) {
     { field: 'stats', headerName: 'Stats', width: 130},
   ]
   props.stats.labels.map((label) => {
-    let labelObj = { field: label, headerName: label, width: 85};
+    let labelObj = { field: label , headerName: label, width: 85};
     columns.push(labelObj);
   })
   const rows = []
   props.stats.splits.map((type, index) => {
     let typeObj = {};
     typeObj['id'] = index;
-    typeObj['stats'] = type.displayName;
+    typeObj['stats'] = type.displayName === 'Regular Season' ? 'Regular' : type.displayName;
     type.stats.map((stat, index) => {
       typeObj[props.stats.labels[index]] = stat;
     })
@@ -38,10 +38,10 @@ export default function PlayerOverview(props) {
     return(
       <div className="player-overview">
       <br></br>
-      <div className="overview-stats" style={{ height: 250, width: '82%', paddingTop: '15px'}}>
+      <div className="overview-stats" style={{ height: 180, width: '82%', paddingTop: '15px'}}>
         <MuiThemeProvider theme={theme}>
           <DataGrid
-            rows={rows} columns={columns} autoPageSize={true} hideFooterPagination={true} 
+            rows={rows} columns={columns} autoPageSize={true} hideFooter={true} 
           />
         </MuiThemeProvider>
       </div>
@@ -56,7 +56,8 @@ export default function PlayerOverview(props) {
             </div>
             <img style={{maxWidth: '5em 2em'}} src={`${props.nextGame.league.events[0].competitors[0].logo}`} alt=""/>
           </div>
-          <h1>  vs  </h1>
+          <h1 >  vs  </h1>
+          <br></br>
           <div className="teams">
             <img style={{maxWidth: '5em 2em'}} src={`${props.nextGame.league.events[0].competitors[1].logo}`} alt=""/>
             <div className="name-record">
