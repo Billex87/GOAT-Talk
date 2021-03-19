ad73778d8e7d622276fecab14c93f6ef1872d5c7
samjanderson <sam.andersonnn@live.ca>
2021-03-18 17:56:07 -0700

    3.5 hours of nothing yay

--- react-front-end/src/components/home.js
+++ react-front-end/src/components/home.js
@@ -7,7 +7,7 @@ export default function Home(props) {
 
   const [playerOneState, setPlayerOneState] = useState({
     playerName: null,
-    playerStats: { "pts": 0, "reb": 0, "ast": 0, "stl": 0, "blk": 0, "fg_pct": 0, "fg3_pct": 0, "ft_pct": 0 },
+    playerStats: { "pts": 0, "reb": 0, "ast": 0, "stl": 0, "blk": 0, "fg_pct": 0, "fg3_pct": 0, "ft_pct": 0, "per": 0, "ts%": 0 },
     season: null,
     firstName: null,
     lastName: null,
@@ -18,7 +18,7 @@ export default function Home(props) {
 
   const [playerTwoState, setPlayerTwoState] = useState({
     playerName: null,
-    playerStats: { "pts": 0, "reb": 0, "ast": 0, "stl": 0, "blk": 0, "fg_pct": 0, "fg3_pct": 0, "ft_pct": 0 },
+    playerStats: { "pts": 0, "reb": 0, "ast": 0, "stl": 0, "blk": 0, "fg_pct": 0, "fg3_pct": 0, "ft_pct": 0, "per": 0, "ts%": 0  },
     season: null,
     firstName: null,
     lastName: null,
@@ -48,7 +48,65 @@ export default function Home(props) {
         } else {
           axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${res.data.data[0].id}`)
             .then(async res => {
-              console.log(res.data.data);
+              
+              // let FTA = Math.round((res.data.data[0].fta * res.data.data[0].games_played)* 100) / 100;
+              // let ThreePTA = Math.round((res.data.data[0].fg3a * res.data.data[0].games_played)* 100) / 100;
+              // let FGA = Math.round((res.data.data[0].fga * res.data.data[0].games_played)* 100) / 100;
+              // let TFGA = Math.round((FGA + ThreePTA)* 100) / 100;
+              // let TSA = (TFGA + 0.44 * FTA);
+              
+              // res.data.data[0]["ts%"] = Math.round(((res.data.data[0].pts * res.data.data[0].games_played) / (TSA * 2))*100* 100) / 100;
+              // calculates TS%
+              res.data.data[0]["ts%"] = Math.round(((res.data.data[0].pts * res.data.data[0].games_played) / (((Math.round(((Math.round((res.data.data[0].fga * res.data.data[0].games_played)* 100) / 100) + (Math.round((res.data.data[0].fg3a * res.data.data[0].games_played)* 100) / 100))* 100) / 100) + 0.44 * (Math.round((res.data.data[0].fta * res.data.data[0].games_played)* 100) / 100)) * 2))*100* 100) / 100;
+              
+              let mins = Math.round((((Number(res.data.data[0].min.split(":")[0]) * 60) + Number(res.data.data[0].min.split(":")[1])) / 60)* 100) / 100;
+              console.log(mins)
+              
+              // calculates PER
+              res.data.data[0].per = Math.round(((
+                (((res.data.data[0].fga * res.data.data[0].games_played) * res.data.data[0].fg_pct) * 85.910) + 
+                ((res.data.data[0].stl * res.data.data[0].games_played) * 53.897) + 
+                (((res.data.data[0].fg3a * res.data.data[0].games_played) * res.data.data[0].fg3_pct) * 51.757) + 
+                (((res.data.data[0].fta * res.data.data[0].games_played) * res.data.data[0].ft_pct) * 46.845) + 
+                ((res.data.data[0].blk * res.data.data[0].games_played) * 39.190) + 
+                ((res.data.data[0].oreb * res.data.data[0].games_played) * 39.190) + 
+                ((res.data.data[0].ast * res.data.data[0].games_played) * 34.677) + 
+                ((res.data.data[0].dreb * res.data.data[0].games_played) * 14.707) -
+                ((res.data.data[0].pf * res.data.data[0].games_played) * 17.174) - 
+                (((res.data.data[0].fta * res.data.data[0].games_played) - (((res.data.data[0].fta * res.data.data[0].games_played) * res.data.data[0].ft_pct))) * 20.091) - 
+                (((res.data.data[0].fga * res.data.data[0].games_played) - (((res.data.data[0].fga * res.data.data[0].games_played) * res.data.data[0].fg_pct))) * 39.190) - 
+                ((res.data.data[0].turnover * res.data.data[0].games_played) * 53.897)) * 
+                (1 / (mins * res.data.data[0].games_played))
+              )* 100) / 100;
+
+              // [ FGM x 85.910
+              // Math.round((((res.data.data[0].fga * res.data.data[0].games_played) * res.data.data[0].fg_pct) * 85.910)* 100) / 100
+              // + Steals x 53.897
+              // Math.round((res.data.data[0].stl * res.data.data[0].games_played) * 53.897)* 100) / 100
+              // + 3PTM x 51.757
+              // Math.round((((res.data.data[0].fg3a * res.data.data[0].games_played) * res.data.data[0].fg3_pct) * 51.757)* 100) / 100
+              // + FTM x 46.845
+              // Math.round((((res.data.data[0].fta * res.data.data[0].games_played) * res.data.data[0].ft_pct) * 46.845)* 100) / 100
+              // + Blocks x 39.190
+              // Math.round(((res.data.data[0].blk * res.data.data[0].games_played) * 39.190)* 100) / 100
+              // + Offensive_Reb x 39.190
+              // Math.round(((rowObj.OR * rowObj.GP) * 39.190)* 100) / 100
+              // + Assists x 34.677
+              // Math.round(((res.data.data[0].ast * res.data.data[0].games_played) * 34.677)* 100) / 100
+              // + Defensive_Reb x 14.707
+              // Math.round(((res.data.data[0].dreb * res.data.data[0].games_played) * 14.707)* 100) / 100
+              // - Foul x 17.174
+              // Math.round(((res.data.data[0].pf * res.data.data[0].games_played) * 17.174)* 100) / 100
+              // - FT_Miss x 20.091
+              // Math.round((((res.data.data[0].fta * res.data.data[0].games_played) - (((res.data.data[0].fta * res.data.data[0].games_played) * res.data.data[0].ft_pct))) * 20.091)* 100) / 100
+              // - FG_Miss x 39.190
+              // Math.round((((res.data.data[0].fga * res.data.data[0].games_played) - (((res.data.data[0].fga * res.data.data[0].games_played) * res.data.data[0].fg_pct))) * 39.190)* 100) / 100
+              // - TO x 53.897 ]
+              // Math.round(((res.data.data[0].turnover * res.data.data[0].games_played) * 53.897)* 100) / 100
+              // x (1 / Minutes)
+              // Math.round((1 / (mins * res.data.data[0].games_played)))* 100) / 100
+
+              console.log(res.data.data[0]);
               setPlayerOneState((prev) => ({
                 ...prev,
                 playerStats: res.data.data[0],
@@ -83,7 +141,30 @@ export default function Home(props) {
         } else {
           axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=${season}&player_ids[]=${res.data.data[0].id}`)
             .then(async res => {
-              console.log(res.data.data);
+              
+              // calculates TS%
+              res.data.data[0]["ts%"] = Math.round(((res.data.data[0].pts * res.data.data[0].games_played) / (((Math.round(((Math.round((res.data.data[0].fga * res.data.data[0].games_played)* 100) / 100) + (Math.round((res.data.data[0].fg3a * res.data.data[0].games_played)* 100) / 100))* 100) / 100) + 0.44 * (Math.round((res.data.data[0].fta * res.data.data[0].games_played)* 100) / 100)) * 2))*100* 100) / 100;
+              
+              let mins = Math.round((((Number(res.data.data[0].min.split(":")[0]) * 60) + Number(res.data.data[0].min.split(":")[1])) / 60)* 100) / 100;
+              console.log(mins)
+              
+              // calculates PER
+              res.data.data[0].per = Math.round(((
+                (((res.data.data[0].fga * res.data.data[0].games_played) * res.data.data[0].fg_pct) * 85.910) + 
+                ((res.data.data[0].stl * res.data.data[0].games_played) * 53.897) + 
+                (((res.data.data[0].fg3a * res.data.data[0].games_played) * res.data.data[0].fg3_pct) * 51.757) + 
+                (((res.data.data[0].fta * res.data.data[0].games_played) * res.data.data[0].ft_pct) * 46.845) + 
+                ((res.data.data[0].blk * res.data.data[0].games_played) * 39.190) + 
+                ((res.data.data[0].oreb * res.data.data[0].games_played) * 39.190) + 
+                ((res.data.data[0].ast * res.data.data[0].games_played) * 34.677) + 
+                ((res.data.data[0].dreb * res.data.data[0].games_played) * 14.707) -
+                ((res.data.data[0].pf * res.data.data[0].games_played) * 17.174) - 
+                (((res.data.data[0].fta * res.data.data[0].games_played) - (((res.data.data[0].fta * res.data.data[0].games_played) * res.data.data[0].ft_pct))) * 20.091) - 
+                (((res.data.data[0].fga * res.data.data[0].games_played) - (((res.data.data[0].fga * res.data.data[0].games_played) * res.data.data[0].fg_pct))) * 39.190) - 
+                ((res.data.data[0].turnover * res.data.data[0].games_played) * 53.897)) * 
+                (1 / (mins * res.data.data[0].games_played))
+              )* 100) / 100;
+
               setPlayerTwoState((prev) => ({
                 ...prev,
                 playerStats: res.data.data[0],
@@ -126,6 +207,9 @@ export default function Home(props) {
           <p>FG%</p>
           <p>3PT%</p>
           <p>FT%</p>
+          <p>PER</p>
+          <p>TS%</p>
+
         </section>
         <Compare reversed={false} winner={showWinner() === playerTwoState.firstName}  playerAwards={"lebron"} playerAwardsImage={"lebron_awards"} playerImage={"playerImage2"} nameStyle={"name2"} yearStyle={"year2"} teamStyle={"team2"} getPlayer={getPlayerTwo} {...playerTwoState} />
       </div>
--- react-front-end/src/components/leaders.js
+++ react-front-end/src/components/leaders.js
@@ -12,28 +12,32 @@ export default function Leaders(props) {
 
   // console.log('looping?')
 
-  const [loading, setLoading] = useState(true)
-  const [leagueLeaders, setleagueLeaders] = useState({})
+  const [loading, setLoading] = useState(true);
+  const [leagueLeaders, setleagueLeaders] = useState({});
 
-  useEffect(() => {axios.get('https://site.web.api.espn.com/apis/common/v3/sports/basketball/nba/statistics/byathlete?region=us&lang=en&contentorigin=espn&isqualified=true&page=1&limit=250&sort=offensive.avgPoints%3Adesc')
-  .then((response) => {
-    setleagueLeaders(prev => ({
-      ...prev,
-      leagueLeaders: response.data
-    }))
-    setLoading(false)
-  })}, [])
+  useEffect(() => {
+    axios.get('https://site.web.api.espn.com/apis/common/v3/sports/basketball/nba/statistics/byathlete?region=us&lang=en&contentorigin=espn&isqualified=true&page=1&limit=250&sort=offensive.avgPoints%3Adesc')
+    .then((response) => {
+      setleagueLeaders(prev => ({
+        ...prev,
+        leagueLeaders: response.data
+      }));
+      setLoading(false);
+    });
+  }, []);
 
-  if(loading) return null;
-  
+  if (loading) return null;
+
   const headers = leagueLeaders.leagueLeaders.categories[1].labels.slice(0, 12);
   const players = leagueLeaders.leagueLeaders.athletes;
 
   const columns = [
-    { field: 'id', hide: true},
-    {field: 'displayName', headerName: 'Name', width: 200, renderCell: (params) => {
-      return(<a style={{textDecoration: 'none', color: 'black'}} href={`/player/${params.row.id}`}><img style={{verticalAlign: 'middle', width: '48px'}} src={params.row.img}/>{params.row.displayName}</a>)
-    }},
+    { field: 'id', hide: true },
+    {
+      field: 'displayName', headerName: 'Name', width: 200, renderCell: (params) => {
+        return (<a style={{ textDecoration: 'none', color: 'black' }} href={`/player/${params.row.id}`}><img style={{ verticalAlign: 'middle', width: '48px' }} src={params.row.img} />{params.row.displayName}</a>);
+      }
+    },
     { field: 'TEAM', headerName: 'Team', width: 75 }
   ];
 
@@ -42,23 +46,23 @@ export default function Leaders(props) {
       field: header, headerName: header, type: 'number', width: 75
     };
     columns.push(headObj);
-  })
+  });
 
   const rows = [];
 
   players.map((player, index1) => {
-    let playerObject = { 
+    let playerObject = {
       id: player.athlete.id,
       displayName: player.athlete.displayName,
       TEAM: player.athlete.teamShortName,
       img: player.athlete.headshot.href
-     };
+    };
     headers.map((header, index2) => {
       playerObject[header] = player.categories[1].values[index2];
-      
-    })
+
+    });
     rows.push(playerObject);
-  })
+  });
 
   const theme = createMuiTheme({
     typography: {
@@ -66,17 +70,19 @@ export default function Leaders(props) {
     },
     palette: {
       type: "dark",
-    }})
+    }
+  });
 
-    console.log(headers)
+  // console.log(headers)
   // console.log(rows)
   // console.log(columns)
 
 
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div style={{ height: 750, width: '85%', paddingLeft: '15px', marginBottom: '15px'}}>
-      <h1><img src={"/images/trophy.png"} alt="logo" className="Trophy" /><br/><br/>Stat Leaders</h1>
+      <div style={{ height: 750, width: '85%', paddingLeft: '15px', marginBottom: '15px' }}>
+        <h1><img src={"/images/trophy.png"} alt="logo" className="Trophy" /></h1>
+        <h1>Stat Leaders</h1>
         <MuiThemeProvider theme={theme}>
           <DataGrid rows={rows} columns={columns} pageSize={12} disableColumnMenu={true} checkboxSelection={false} />
         </MuiThemeProvider>
